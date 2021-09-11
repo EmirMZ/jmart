3c64eb319aade179df14b141cc6b1eecfbf6af94
Emir Muhamad Zaid <75107919+Zocom59@users.noreply.github.com>
2021-09-11 09:53:52 +0700

    4th commit

---
+++ EmirMuhamadZaidJmartAK/Jmart.java
@@ -0,0 +1,35 @@
+package EmirMuhamadZaidJmartAK;
+
+
+/**
+ * Write a description of class Jmart here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Jmart
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Jmart
+     */
+    public Jmart()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
