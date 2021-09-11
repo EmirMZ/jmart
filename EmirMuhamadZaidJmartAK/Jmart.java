8665ca5d421286c3538a59bcbedb00374cf5d3c4
[Emir Muhamad Zaid] <[emir.muhamad@ui.ac.id]>
2021-09-11 13:38:13 +0700

    final commit

--- EmirMuhamadZaidJmartAK/Jmart.java
+++ EmirMuhamadZaidJmartAK/Jmart.java
@@ -13,36 +13,60 @@ public class Jmart
 {
     public static void main(String[] args) {
         System.out.println("getPromo() = " + getPromo());
+        
         System.out.println("getCustomer() = " + getCustomer());
-        System.out.println("getDiscountPercentage(100,50) = " + getDiscountPercentage(100,50));
-        System.out.println("getDiscountedPrice(2500,60.0f) = " + getDiscountedPrice(100,60.0f));
-        System.out.println(getPromo());
+        
+        System.out.println("getDiscountPercentage(1000,900) = " + getDiscountPercentage(1000,900));
+        System.out.println("getDiscountPercentage(1000,0) = " + getDiscountPercentage(1000,0));
+        System.out.println("getDiscountPercentage(0,0) = " + getDiscountPercentage(0,0));
+        System.out.println("getDiscountPercentage(0,1) = " + getDiscountPercentage(0,1));
+        
+        System.out.println("getDiscountedPrice(1000,10.0f) = " + getDiscountedPrice(1000,10.0f));
+        System.out.println("getDiscountedPrice(1000,100.0f) = " + getDiscountedPrice(1000,100.0f));
+        System.out.println("getDiscountedPrice(1000,120.0f) = " + getDiscountedPrice(1000,120.0f));
+        System.out.println("getDiscountedPrice(0,0.0f) = " + getDiscountedPrice(0,0.0f));
+        
+        System.out.println("getOriginalPrice(900,10.0f) = " + getOriginalPrice(900,10.0f));
+        System.out.println("getOriginalPrice(1000,0.0f) = " + getOriginalPrice(1000,0.0f));
+        System.out.println("getOriginalPrice(0,100.0f) = " + getOriginalPrice(0,100.0f));
+        System.out.println("getOriginalPrice(0,120.0f) = " + getOriginalPrice(0,120.0f));
+        
+        System.out.println("getCommissionMultiplier() = " + getCommissionMultiplier());
+        
+        System.out.println("getAdjustedPrice(1000) = " +  getAdjustedPrice(1000));
+        System.out.println("getAdjustedPrice(500) = " +  getAdjustedPrice(500));
+        System.out.println("getAdjustedPrice(0) = " +  getAdjustedPrice(0));
+        
+        System.out.println("getAdminFee(1000) = " + getAdminFee(1000));
+        System.out.println("getAdminFee(500) = " + getAdminFee(500));
+        System.out.println("getAdminFee(0) = " + getAdminFee(0));
+        
         System.out.println(getPromo());
     }
     
     static int getPromo() {
-       //System.out.println("I just got executed!");
+       
         return 0;
     }
    
     static String getCustomer() {
         
-        //System.out.println("I just got executed!");
-        return "OOP";
+
+        return "oop";
     }
     
     static float getDiscountPercentage(int before, int after) {
         float discount;
         if(before>after){
-            discount = (float)after/(float)before;
-            //System.out.println("here " + after +"test " + before);
+            discount = (float)(((float)(before)-(float)after)/(float)before);
+           
             discount = discount*100;
-            //System.out.println("here2 "+discount);
+
         }else{
-            //System.out.println("nope");
+
             discount = 0;
+            
         }
-        //System.out.println("I just got executed!");
         return discount;
     }
 
@@ -53,27 +77,31 @@ public class Jmart
         if (discountPercentage > 100.0f){
             discountPercentage = 100.0f;
         }
-        
-        floatPrice = (float)price ;
-        floatPrice = discountPercentage/(100.0f * floatPrice);
-        finalPrice = (int)floatPrice;
-        return finalPrice;
-        //System.out.println("I just got executed!");
-    }
 
-    static void getOriginalPrice() {
+        return (int)((float)(price)-((float)price)*((float)discountPercentage/100.0f));
+     }
+
+    static int getOriginalPrice(int discountedPrice, float discountPercentage) {
+        if (discountPercentage > 100.0f){
+            discountPercentage = 100.0f;
+        }
+        return (int)((float)(discountedPrice * 100.0f)/(100.0f-(float)discountPercentage));
         //System.out.println("I just got executed!");
     }
      
-    static void getCommissionMultiplier() {
-        System.out.println("I just got executed!");
+    static float getCommissionMultiplier() {
+        return 0.05f;
     }
     
-    static void getAdjustedPrice() {
-        System.out.println("I just got executed!");
+    static int getAdjustedPrice(int price) {
+        price = (int)((float)(getCommissionMultiplier() * price) + (float)price);
+        //System.out.println("I just got executed!");
+        return price;
     }
 
-    static void getAdminFee() {
-        System.out.println("I just got executed!");
+    static int getAdminFee(int price) {
+        //System.out.println("I just got executed!");
+        price = (int)((float)(getCommissionMultiplier() * price));
+        return price;
     }
 }
