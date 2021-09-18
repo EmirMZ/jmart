2fc5b390b623c5c3377835c3b5cc7828500ec4c2
[Emir Muhamad Zaid] <[emir.muhamad@ui.ac.id]>
2021-09-18 10:57:21 +0700

    Modul2again

---
+++ EmirMuhamadZaidJmartAK/ProductClass.java
@@ -0,0 +1,11 @@
+package EmirMuhamadZaidJmartAK;
+
+// + = public
+// - = private
+// _ = static
+
+public class ProductClass{
+    public void Product(String name, int weight, boolean conditionUsed,PriceTag priceTag, ProductCategory category){
+        
+    }
+}
--- EmirMuhamadZaidJmartAK/ProductRating.java
+++ EmirMuhamadZaidJmartAK/ProductRating.java
@@ -35,10 +35,5 @@ public class ProductRating{
     public long getTotal(){
         return this.total;
     }
-
-    enum ProductCategory{
-        BOOK, KITCHEN, ELECTRONIC, FASHION, GAMING, GADGET, MOTHERCARE, COSMETICS, HEALTHCARE, FURNITURE, JEWELRY, TOYS, FNB, STATIONERY, 
-        SPORTS, AUTOMOTIVE, PETCARE, ART_CRAFT, CARPENTRY, MISCELLANEOUS, PROPERTY, TRAVEL, WEDDING
-    }
     
 }
